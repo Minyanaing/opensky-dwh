ced961d8b5225250bcd982b8405a4c3659d7a00d
Minyanaing <yanaing.EC.1997@gmail.com>
2026-07-23 09:50:35 +0700

    Fix copy into sql

--- de-pipeline/databricks/landing/sql/flights_raw.sql
+++ de-pipeline/databricks/landing/sql/flights_raw.sql
@@ -1,6 +1,22 @@
 COPY INTO opensky_raw.bronze.flights_raw
 FROM (
-  SELECT *, current_timestamp() AS _loaded_at
+  SELECT
+    icao24,
+    callsign,
+    estDepartureAirport,
+    estArrivalAirport,
+    CAST(firstSeen AS BIGINT) AS firstSeen,
+    CAST(lastSeen AS BIGINT) AS lastSeen,
+    CAST(estDepartureAirportHorizDistance AS BIGINT) AS estDepartureAirportHorizDistance,
+    CAST(estDepartureAirportVertDistance AS BIGINT) AS estDepartureAirportVertDistance,
+    CAST(estArrivalAirportHorizDistance AS BIGINT) AS estArrivalAirportHorizDistance,
+    CAST(estArrivalAirportVertDistance AS BIGINT) AS estArrivalAirportVertDistance,
+    CAST(departureAirportCandidatesCount AS BIGINT) AS departureAirportCandidatesCount,
+    CAST(arrivalAirportCandidatesCount AS BIGINT) AS arrivalAirportCandidatesCount,
+    queried_airport,
+    movement_type,
+    fetched_at,
+    current_timestamp() AS _loaded_at
   FROM '/Volumes/opensky_raw/bronze/landing/flights_raw/'
 )
 FILEFORMAT = CSV
