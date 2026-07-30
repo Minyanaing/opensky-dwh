01581a714e945a2fc39ebb02ad7772cda160bd14
Minyanaing <yanaing.EC.1997@gmail.com>
2026-07-30 16:45:25 +0700

    Table drop scripts for master silver gold

---
+++ de-pipeline/databricks/env_setup/destroy_master.sql
@@ -0,0 +1,4 @@
+-- {{CATALOG}} substituted per environment by env_setup.py --catalog. Drops the tables created by
+-- tables_master.sql - schema/catalog untouched.
+DROP TABLE IF EXISTS {{CATALOG}}.master.date;
+DROP TABLE IF EXISTS {{CATALOG}}.master.time;
